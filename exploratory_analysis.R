b4ce9ceae9a58e190930e92493c2f310e7a98b86
mazed <mazedur@gmail.com>
2022-02-20 01:34:40 +0600

    Remdial Outlier & Missing Values

--- exploratory_analysis.R
+++ exploratory_analysis.R
@@ -14,3 +14,27 @@ pairs(~price+crime_rate+n_hos_beds+n_hot_rooms+rainfall,data = df)
 barplot(table(df$airport))
 barplot(table(df$waterbody))
 barplot(table(df$bus_ter))
+
+# Observations
+# Outliers Detected : rainfalls & n_hot_rooms
+# Missing Value : n_host_beds
+# Useless Variable : bus_ter
+# Bi-variate Relationship : Price & Crime Rate ( Negative )
+
+# Outlier Remedial
+quantile(df$n_hot_rooms, 0.99)
+uv <- 3 * quantile(df$n_hot_rooms, 0.99)
+df$n_hot_rooms[df$n_hot_rooms > uv] <- uv
+summary(df$n_hot_rooms)
+
+summary(df$rainfall)
+lv = 0.3 * quantile(df$rainfall, 0.01)
+df$rainfall[df$rainfall < lv] <- lv
+summary(df$rainfall)
+
+# Handling Missing Value
+mean(df$n_hos_beds)
+mean(df$n_hos_beds, na.rm = TRUE)
+which(is.na(df$n_hos_beds)) 
+df$n_hos_beds[is.na(df$n_hos_beds)] <- mean(df$n_hos_beds, na.rm = TRUE)
+mean(df$n_hos_beds)
